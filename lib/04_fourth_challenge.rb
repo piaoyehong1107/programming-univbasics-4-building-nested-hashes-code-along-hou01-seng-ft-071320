509874ef67cb8bd2b5706f0400e26a5404563093
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-28 04:52:19 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -5,11 +5,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {:name =>"Benvolio"
-         :age =>"17"
+        {:name =>"Benvolio",
+         :age =>"17",
          :attitude =>"worried"},
-        {:name =>"Mercutio"
-         :age =>"18"
+        {:name =>"Mercutio",
+         :age =>"18",
          :attitude =>"hot-headed"}
       ]
    }, 
@@ -18,11 +18,11 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {:name =>"Steven"
-         :age =>"30"
+        {:name =>"Steven",
+         :age =>"30",
          :attitude =>"confused"},
-        {:name => "Nurse"
-         :age =>"44"
+        {:name => "Nurse",
+         :age =>"44",
          :attitude =>"worried"}
         ]
    }
